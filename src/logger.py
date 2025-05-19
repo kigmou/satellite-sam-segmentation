a5533f7b7f3d9f5e5afad6b8109d2bde6ceeed31
kigmou <bilal.kigmou@etu.unistra.fr>
2025-05-19 11:38:30 +0200

    Fix console logs

--- src/logger.py
+++ src/logger.py
@@ -9,12 +9,17 @@ def configure_logger(name="logger", is_file=False):
     logger.setLevel(logging.INFO)
 
     if not logger.handlers:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
         if is_file:
             os.makedirs('logs', exist_ok=True)
             log_filename = datetime.strftime(datetime.now(), 'logs/logs_%Y%m%d_%H%M%S.log')
             file_handler = logging.FileHandler(log_filename)
             file_handler.setLevel(logging.INFO)
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
             file_handler.setFormatter(formatter)
             logger.addHandler(file_handler)
 
